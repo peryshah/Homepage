03e1e0d0dc9b5eae6896475be4303bc2601effc1
peryshah <97086541+peryshah@users.noreply.github.com>
2024-06-01 11:59:46 +0530

    Add files via upload

--- homepage.js
+++ homepage.js
@@ -220,430 +220,72 @@ searchBtn.addEventListener("click", () => {
 checkWeather();
 
 // JS for Games///////////////////////////////////////////////////////////////////////////////
-// Js for Tic Tac Toe........................................................................
-// Function called whenever user tab on any box
-function myfunc() {
 
-	// Setting DOM to all boxes or input field
-	var b1, b2, b3, b4, b5, b6, b7, b8, b9;
-	b1 = document.getElementById("b1").value;
-	b2 = document.getElementById("b2").value;
-	b3 = document.getElementById("b3").value;
-	b4 = document.getElementById("b4").value;
-	b5 = document.getElementById("b5").value;
-	b6 = document.getElementById("b6").value;
-	b7 = document.getElementById("b7").value;
-	b8 = document.getElementById("b8").value;
-	b9 = document.getElementById("b9").value;
 
-	var b1btn, b2btn, b3btn, b4btn, b5btn,
-		b6btn, b7btn, b8btn, b9btn;
-		
-	b1btn = document.getElementById("b1");
-	b2btn = document.getElementById("b2");
-	b3btn = document.getElementById("b3");
-	b4btn = document.getElementById("b4");
-	b5btn = document.getElementById("b5");
-	b6btn = document.getElementById("b6");
-	b7btn = document.getElementById("b7");
-	b8btn = document.getElementById("b8");
-	b9btn = document.getElementById("b9");
 
-	// Checking if Player X won or not and after
-	// that disabled all the other fields
-	if ((b1 == 'x' || b1 == 'X') && (b2 == 'x' ||
-		b2 == 'X') && (b3 == 'x' || b3 == 'X')) {
-		document.getElementById('print')
-			.innerHTML = "Player X won";
-		b4btn.disabled = true;
-		b5btn.disabled = true;
-		b6btn.disabled = true;
-		b7btn.disabled = true;
-		b8btn.disabled = true;
-		b9btn.disabled = true;
+// JS for Youtube downloader Starts /////////////////////////////////////////////////////////
 
-		b1btn.style.color = "red";
-		b2btn.style.color = "red";
-		b3btn.style.color = "red";
-	}
-	else if ((b1 == 'x' || b1 == 'X') && (b4 == 'x' ||
-		b4 == 'X') && (b7 == 'x' || b7 == 'X')) {
-		document.getElementById('print')
-			.innerHTML = "Player X won";
-		b2btn.disabled = true;
-		b3btn.disabled = true;
-		b5btn.disabled = true;
-		b6btn.disabled = true;
-		b8btn.disabled = true;
-		b9btn.disabled = true;
 
-		b1btn.style.color = "red";
-		b4btn.style.color = "red";
-		b7btn.style.color = "red";
-	}
-	else if ((b7 == 'x' || b7 == 'X') && (b8 == 'x' ||
-		b8 == 'X') && (b9 == 'x' || b9 == 'X')) {
-		document.getElementById('print')
-			.innerHTML = "Player X won";
 
-		b1btn.disabled = true;
-		b2btn.disabled = true;
-		b3btn.disabled = true;
-		b4btn.disabled = true;
-		b5btn.disabled = true;
-		b6btn.disabled = true;
+const express = require('express');
+const app = express();
+const { exec } = require('child_process');
 
-		b7btn.style.color = "red";
-		b8btn.style.color = "red";
-		b9btn.style.color = "red";
-	}
-	else if ((b3 == 'x' || b3 == 'X') && (b6 == 'x' ||
-		b6 == 'X') && (b9 == 'x' || b9 == 'X')) {
-		document.getElementById('print')
-			.innerHTML = "Player X won";
+// Handle GET request to initiate video download
+app.get('/api/download', (req, res) => {
+    const videoUrl = req.query.url;
+    if (!videoUrl) {
+        return res.status(400).json({ success: false, error: 'Invalid video URL' });
+    }
 
-		b1btn.disabled = true;
-		b2btn.disabled = true;
-		b4btn.disabled = true;
-		b5btn.disabled = true;
-		b7btn.disabled = true;
-		b8btn.disabled = true;
+    // Use youtube-dl to download the video
+    const command = `youtube-dl -f bestvideo+bestaudio --merge-output-format mp4 ${videoUrl}`;
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error('Error:', error);
+            return res.status(500).json({ success: false, error: 'Failed to download video' });
+        }
 
-		b3btn.style.color = "red";
-		b6btn.style.color = "red";
-		b9btn.style.color = "red";
-	}
-	else if ((b1 == 'x' || b1 == 'X') && (b5 == 'x' ||
-		b5 == 'X') && (b9 == 'x' || b9 == 'X')) {
-		document.getElementById('print')
-			.innerHTML = "Player X won";
-		b2btn.disabled = true;
-		b3btn.disabled = true;
-		b4btn.disabled = true;
-		b6btn.disabled = true;
-		b7btn.disabled = true;
-		b8btn.disabled = true;
+        console.log('Video downloaded:', stdout);
+        res.json({ success: true });
+    });
+});
 
-		b1btn.style.color = "red";
-		b5btn.style.color = "red";
-		b9btn.style.color = "red";
-	}
-	else if ((b3 == 'x' || b3 == 'X') && (b5 == 'x' ||
-		b5 == 'X') && (b7 == 'x' || b7 == 'X')) {
-		document.getElementById('print')
-			.innerHTML = "Player X won";
-		b1btn.disabled = true;
-		b2btn.disabled = true;
-		b4btn.disabled = true;
-		b6btn.disabled = true;
-		b8btn.disabled = true;
-		b9btn.disabled = true;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
-		b3btn.style.color = "red";
-		b5btn.style.color = "red";
-		b7btn.style.color = "red";
-	}
-	else if ((b2 == 'x' || b2 == 'X') && (b5 == 'x' ||
-		b5 == 'X') && (b8 == 'x' || b8 == 'X')) {
-		document.getElementById('print')
-			.innerHTML = "Player X won";
-		b1btn.disabled = true;
-		b2btn.disabled = true;
-		b4btn.disabled = true;
-		b6btn.disabled = true;
-		b7btn.disabled = true;
-		b9btn.disabled = true;
 
-		b2btn.style.color = "red";
-		b5btn.style.color = "red";
-		b8btn.style.color = "red";
-	}
-	else if ((b4 == 'x' || b4 == 'X') && (b5 == 'x' ||
-		b5 == 'X') && (b6 == 'x' || b6 == 'X')) {
-		document.getElementById('print')
-			.innerHTML = "Player X won";
-		b1btn.disabled = true;
-		b2btn.disabled = true;
-		b3btn.disabled = true;
-		b7btn.disabled = true;
-		b8btn.disabled = true;
-		b9btn.disabled = true;
-
-		b4btn.style.color = "red";
-		b5btn.style.color = "red";
-		b6btn.style.color = "red";
+function downloadVideo() {
+	const videoUrl = document.getElementById('videoUrlInput').value;
+	if (videoUrl.trim() === '') {
+		alert('Please enter a valid YouTube video URL.');
+		return;
 	}
 
-	// Checking of Player X finish
-	// Checking for Player 0 starts, Is player 0 won or
-	// not and after that disabled all the other fields
-	else if ((b1 == '0' || b1 == '0') && (b2 == '0' ||
-		b2 == '0') && (b3 == '0' || b3 == '0')) {
-		document.getElementById('print')
-			.innerHTML = "Player 0 won";
-		b4btn.disabled = true;
-		b5btn.disabled = true;
-		b6btn.disabled = true;
-		b7btn.disabled = true;
-		b8btn.disabled = true;
-		b9btn.disabled = true;
-
-		b1btn.style.color = "red";
-		b2btn.style.color = "red";
-		b3btn.style.color = "red";
-	}
-	else if ((b1 == '0' || b1 == '0') && (b4 == '0' ||
-		b4 == '0') && (b7 == '0' || b7 == '0')) {
-		document.getElementById('print')
-			.innerHTML = "Player 0 won";
-		b2btn.disabled = true;
-		b3btn.disabled = true;
-		b5btn.disabled = true;
-		b6btn.disabled = true;
-		b8btn.disabled = true;
-		b9btn.disabled = true;
-
-		b1btn.style.color = "red";
-		b4btn.style.color = "red";
-		b7btn.style.color = "red";
-	}
-	else if ((b7 == '0' || b7 == '0') && (b8 == '0' ||
-		b8 == '0') && (b9 == '0' || b9 == '0')) {
-		document.getElementById('print')
-			.innerHTML = "Player 0 won";
-		b1btn.disabled = true;
-		b2btn.disabled = true;
-		b3btn.disabled = true;
-		b4btn.disabled = true;
-		b5btn.disabled = true;
-		b6btn.disabled = true;
-
-		b7btn.style.color = "red";
-		b8btn.style.color = "red";
-		b9btn.style.color = "red";
-	}
-	else if ((b3 == '0' || b3 == '0') && (b6 == '0' ||
-		b6 == '0') && (b9 == '0' || b9 == '0')) {
-		document.getElementById('print')
-			.innerHTML = "Player 0 won";
-		b1btn.disabled = true;
-		b2btn.disabled = true;
-		b4btn.disabled = true;
-		b5btn.disabled = true;
-		b7btn.disabled = true;
-		b8btn.disabled = true;
-		b3btn.style.color = "red";
-		b6btn.style.color = "red";
-		b9btn.style.color = "red";
-	}
-	else if ((b1 == '0' || b1 == '0') && (b5 == '0' ||
-		b5 == '0') && (b9 == '0' || b9 == '0')) {
-		document.getElementById('print')
-			.innerHTML = "Player 0 won";
-		b2btn.disabled = true;
-		b3btn.disabled = true;
-		b4btn.disabled = true;
-		b6btn.disabled = true;
-		b7btn.disabled = true;
-		b8btn.disabled = true;
-
-		b1btn.style.color = "red";
-		b5btn.style.color = "red";
-		b9btn.style.color = "red";
-	}
-	else if ((b3 == '0' || b3 == '0') && (b5 == '0' ||
-		b5 == '0') && (b7 == '0' || b7 == '0')) {
-		document.getElementById('print')
-			.innerHTML = "Player 0 won";
-		b1btn.disabled = true;
-		b2btn.disabled = true;
-		b4btn.disabled = true;
-		b6btn.disabled = true;
-		b8btn.disabled = true;
-		b9btn.disabled = true;
-
-		b3btn.style.color = "red";
-		b5btn.style.color = "red";
-		b7btn.style.color = "red";
-	}
-	else if ((b2 == '0' || b2 == '0') && (b5 == '0' ||
-		b5 == '0') && (b8 == '0' || b8 == '0')) {
-		document.getElementById('print')
-			.innerHTML = "Player 0 won";
-		b1btn.disabled = true;
-		b3btn.disabled = true;
-		b4btn.disabled = true;
-		b6btn.disabled = true;
-		b7btn.disabled = true;
-		b9btn.disabled = true;
-
-		b2btn.style.color = "red";
-		b5btn.style.color = "red";
-		b8btn.style.color = "red";
-	}
-	else if ((b4 == '0' || b4 == '0') && (b5 == '0' ||
-		b5 == '0') && (b6 == '0' || b6 == '0')) {
-		document.getElementById('print')
-			.innerHTML = "Player 0 won";
-		b1btn.disabled = true;
-		b2btn.disabled = true;
-		b3btn.disabled = true;
-		b7btn.disabled = true;
-		b8btn.disabled = true;
-		b9btn.disabled = true;
-
-		b4btn.style.color = "red";
-		b5btn.style.color = "red";
-		b6btn.style.color = "red";
-	}
-
-	// Checking of Player 0 finish
-	// Here, Checking about Tie
-	else if ((b1 == 'X' || b1 == '0') && (b2 == 'X'
-		|| b2 == '0') && (b3 == 'X' || b3 == '0') &&
-		(b4 == 'X' || b4 == '0') && (b5 == 'X' ||
-			b5 == '0') && (b6 == 'X' || b6 == '0') &&
-		(b7 == 'X' || b7 == '0') && (b8 == 'X' ||
-			b8 == '0') && (b9 == 'X' || b9 == '0')) {
-		document.getElementById('print')
-			.innerHTML = "Match Tie";
-	}
-	else {
-
-		// Here, Printing Result
-		if (flag == 1) {
-			document.getElementById('print')
-				.innerHTML = "Player X Turn";
-		}
-		else {
-			document.getElementById('print')
-				.innerHTML = "Player 0 Turn";
-		}
-	}
+	// Call a backend API to initiate the video download
+	fetch(`/api/download?url=${encodeURIComponent(videoUrl)}`)
+		.then(response => response.json())
+		.then(data => {
+			if (data.success) {
+				alert('Video download initiated successfully.');
+			} else {
+				alert('Failed to initiate video download. Please try again.');
+			}
+		})
+		.catch(error => {
+			console.error('Error:', error);
+			alert('An error occurred. Please try again.');
+		});
 }
+// JS for Youtube downloader ends /////////////////////////////////////////////////////////
 
-// Function to reset game
-function myfunc_2() {
-	location.reload();
-	b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = '';
-}
 
-// Here onwards, functions check turn of the player
-// and put accordingly value X or 0
-flag = 1;
-function myfunc_3() {
-	if (flag == 1) {
-		document.getElementById("b1").value = "X";
-		document.getElementById("b1").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b1").value = "0";
-		document.getElementById("b1").disabled = true;
-		flag = 1;
-	}
-}
 
-function myfunc_4() {
-	if (flag == 1) {
-		document.getElementById("b2").value = "X";
-		document.getElementById("b2").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b2").value = "0";
-		document.getElementById("b2").disabled = true;
-		flag = 1;
-	}
-}
+// JS for SUKUDU Starts ///////////////////////////////////////////////////////////////////////////
 
-function myfunc_5() {
-	if (flag == 1) {
-		document.getElementById("b3").value = "X";
-		document.getElementById("b3").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b3").value = "0";
-		document.getElementById("b3").disabled = true;
-		flag = 1;
-	}
-}
 
-function myfunc_6() {
-	if (flag == 1) {
-		document.getElementById("b4").value = "X";
-		document.getElementById("b4").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b4").value = "0";
-		document.getElementById("b4").disabled = true;
-		flag = 1;
-	}
-}
 
-function myfunc_7() {
-	if (flag == 1) {
-		document.getElementById("b5").value = "X";
-		document.getElementById("b5").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b5").value = "0";
-		document.getElementById("b5").disabled = true;
-		flag = 1;
-	}
-}
 
-function myfunc_8() {
-	if (flag == 1) {
-		document.getElementById("b6").value = "X";
-		document.getElementById("b6").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b6").value = "0";
-		document.getElementById("b6").disabled = true;
-		flag = 1;
-	}
-}
-
-function myfunc_9() {
-	if (flag == 1) {
-		document.getElementById("b7").value = "X";
-		document.getElementById("b7").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b7").value = "0";
-		document.getElementById("b7").disabled = true;
-		flag = 1;
-	}
-}
-
-function myfunc_10() {
-	if (flag == 1) {
-		document.getElementById("b8").value = "X";
-		document.getElementById("b8").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b8").value = "0";
-		document.getElementById("b8").disabled = true;
-		flag = 1;
-	}
-}
-
-function myfunc_11() {
-	if (flag == 1) {
-		document.getElementById("b9").value = "X";
-		document.getElementById("b9").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b9").value = "0";
-		document.getElementById("b9").disabled = true;
-		flag = 1;
-	}
-}
+// JS for SUDUKU Ends/////////////////////////////////////////////////////////////////////////////
